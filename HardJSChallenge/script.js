cffb5acc7092f23454deed3ac9763bcaf4ba6ae3
arodsalas <114959882+arodsalas@users.noreply.github.com>
2023-01-09 12:52:11 -0500

    Add files via upload

---
+++ HardJSChallenge/script.js
@@ -0,0 +1,47 @@
+var first = 10
+var second = 2
+var total;
+
+console.log(first + second)
+
+var first = 5
+var second = 4
+var total;
+
+console.log(first + second)
+
+var first = 5
+var second = 4
+var total;
+
+console.log(first - second)
+
+var first = 20
+var second = 7
+var total;
+
+console.log(first - second)
+
+var first = 10
+var second = 4
+var total;
+
+console.log(first * second)
+
+var first = 3
+var second = 5
+var total;
+
+console.log(first * second)
+
+var first = 30
+var second = 5
+var total;
+
+console.log(first / second)
+
+var first = 20
+var second = 4
+var total;
+
+console.log(first / second)
